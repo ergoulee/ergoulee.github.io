0142ecf7a5f43698ff59dff81bf4b102da8f993f
lee <76640081@qq.com>
2019-10-30 17:21:26 +0800

    github actions

---
+++ README.md
@@ -0,0 +1,8 @@
+# Landing create-react-app Example
+
+详细使用请查看 landing 里的 [use in create-react-app](https://landing.ant.design/docs/use/create-react-app)
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+# 李敬知的主页
+10-30 测试github Actions
---
+++ src/Home/data.source.js
@@ -0,0 +1,29 @@
+import React from 'react';
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0 jy53s0ipjy-editor_css' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title',
+    children: (
+      <>
+        <p>李敬知</p>
+      </>
+    ),
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <>
+        <p>快点长大，长大了自己维护。</p>
+      </>
+    ),
+  },
+  button: {
+    className: 'banner0-button',
+    children: (
+      <>
+        <p>更多</p>
+      </>
+    ),
+  },
+};
